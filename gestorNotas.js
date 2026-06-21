e1a39c75217be864ad66f5c755db1985d3fa3f65
Sandra <hi-sandra@hotmail.com>
2026-06-20 20:19:31 -0600

    nueva nota

--- gestorNotas.js
+++ gestorNotas.js
@@ -8,3 +8,10 @@ function agregarNota(titulo, contenido) {
     const data = fs.readFileSync(filePath, 'utf8');
     notas = JSON.parse(data); 
   }
+
+const nuevaNota = { titulo, contenido };
+  notas.push(nuevaNota);
+  fs.writeFileSync(filePath, JSON.stringify(notas, null, 2));
+  console.log('Nota agregada con éxito.');
+}
+
