c7fffaa70d8bbbbbc0b9ec7f89f08cdc02faf072
Sandra <hi-sandra@hotmail.com>
2026-06-20 20:21:56 -0600

    eliminar nota

--- gestorNotas.js
+++ gestorNotas.js
@@ -24,3 +24,17 @@ function listarNotas() {
     console.log('No hay notas guardadas.');
   }
 }
+
+function eliminarNota(titulo) {
+  if (fs.existsSync(filePath)) {
+    const data = fs.readFileSync(filePath, 'utf8');
+    const notas = JSON.parse(data);
+
+    const notasRestantes = notas.filter((nota) => nota.titulo !== titulo);
+
+    fs.writeFileSync(filePath, JSON.stringify(notasRestantes, null, 2));
+    console.log(`Nota con título "${titulo}" eliminada.`);
+  } else {
+    console.log('No hay notas para eliminar.');
+  }
+}
