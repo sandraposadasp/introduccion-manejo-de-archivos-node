ebfac02c2c7fa2a2574d7f8659ce8ddafb62ff0b
Sandra <hi-sandra@hotmail.com>
2026-06-20 20:18:10 -0600

    agregar nota

--- gestorNotas.js
+++ gestorNotas.js
@@ -1,3 +1,10 @@
 const fs = require('fs');
 
 const filePath = './notas.json';
+
+function agregarNota(titulo, contenido) {
+  let notas = [];
+  if (fs.existsSync(filePath)) {
+    const data = fs.readFileSync(filePath, 'utf8');
+    notas = JSON.parse(data); 
+  }
